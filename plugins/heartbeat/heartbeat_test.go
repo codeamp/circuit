dad1d4dce97fd7dc6009a47b9fb539456c8b2a23
Jeremy Deininger <jeremydeininger@gmail.com>
2017-11-28 14:40:28 -0800

    fixing heartbeat test

--- plugins/heartbeat/heartbeat_test.go
+++ plugins/heartbeat/heartbeat_test.go
@@ -1,27 +1,37 @@
 package heartbeat_test
 
 import (
+	"bytes"
 	"testing"
 
 	"github.com/codeamp/circuit/plugins"
 	"github.com/codeamp/transistor"
+	"github.com/spf13/viper"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/suite"
 )
 
 type TestSuite struct {
 	suite.Suite
-	transistor *transistor.Agent
+	transistor *transistor.Transistor
 }
 
-var config = []byte(`
+var viperConfig = []byte(`
 plugins:
   heartbeat:
-    workers: 0
+    workers: 1
 `)
 
 func (suite *TestSuite) SetupSuite() {
-	ag, _ := transistor.NewTesttransistor(config)
+	viper.SetConfigType("YAML")
+	viper.ReadConfig(bytes.NewBuffer(viperConfig))
+	viper.SetConfigType("yaml") // or viper.SetConfigType("YAML")
+
+	config := transistor.Config{
+		Plugins:        viper.GetStringMap("plugins"),
+		EnabledPlugins: []string{"heartbeat"},
+	}
+	ag, _ := transistor.NewTestTransistor(config)
 	suite.transistor = ag
 	go suite.transistor.Run()
 }
