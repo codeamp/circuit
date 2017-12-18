bb054f69f3b56b7c9ec4d81a3834c3f57a9aedf8
Shreyas Jaganmohan <shreyas@checkr.com>
2017-12-18 14:34:28 -0800

    Add Convert fns for MapStringInterrface

--- plugins/utils.go
+++ plugins/utils.go
@@ -12,10 +12,21 @@ type KeyValue struct {
 }
 
 func ConvertMapStringInterfaceToKV(formSpecMap map[string]interface{}, kv *[]KeyValue) error {
+	for key, value := range formSpecMap {
+		*kv = append(*kv, KeyValue{Key: key, Value: value.(string)})
+	}
 	return nil
 }
 
 func ConvertKVToMapStringInterface(kv []KeyValue, formSpecMap *map[string]interface{}) error {
+	formMap := *formSpecMap
+
+	for _, keyValue := range kv {
+		tmpKv := keyValue
+		formMap[tmpKv.Key] = &tmpKv.Value
+	}
+
+	formSpecMap = &formMap
 	return nil
 }
 
