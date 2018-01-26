1defd0dd4fb49d8dbdb4e0a0383fbc5beba00c00
Amos Kyler <amos.kyler@gmail.com>
2018-01-25 17:18:36 -0800

    Secret env vars (#35)

--- plugins/codeamp/models/models.go
+++ plugins/codeamp/models/models.go
@@ -31,8 +31,8 @@ type UserPermission struct {
 }
 
 type Environment struct {
-	Model     `json:",inline"`
-	Name      string `json:"name"`
+	Model `json:",inline"`
+	Name  string `json:"name"`
 }
 
 type EnvironmentVariableScope string
@@ -63,6 +63,7 @@ type EnvironmentVariable struct {
 	ProjectId     uuid.UUID                `bson:"projectId" json:"projectId" gorm:"type:uuid"`
 	Scope         EnvironmentVariableScope `json:"scope"`
 	EnvironmentId uuid.UUID                `bson:"environmentId" json:"environmentId" gorm:"type:uuid"`
+	IsSecret      bool                     `json:"isSecret"`
 }
 
 type EnvironmentVariableValue struct {
@@ -137,10 +138,10 @@ type GitBranch struct {
 
 // This branch is tied to an environment and project
 type EnvironmentBasedProjectBranch struct {
-	Model `json:"inline"`
-	EnvironmentId uuid.UUID    `bson:"environmentId" json:"environmentId" gorm:"type:uuid"`
-	ProjectId     uuid.UUID    `bson:"projectId" json:"projectId" gorm:"type:uuid"`
-	GitBranch string `json:"gitBranch"`
+	Model         `json:"inline"`
+	EnvironmentId uuid.UUID `bson:"environmentId" json:"environmentId" gorm:"type:uuid"`
+	ProjectId     uuid.UUID `bson:"projectId" json:"projectId" gorm:"type:uuid"`
+	GitBranch     string    `json:"gitBranch"`
 }
 
 type Release struct {
@@ -155,7 +156,7 @@ type Release struct {
 	HeadFeatureID uuid.UUID `json:"headFeatureId" gorm:"type:uuid"`
 	TailFeature   Feature
 	TailFeatureID uuid.UUID             `json:"tailFeatureId" gorm:"type:uuid"`
-	Secrets       []EnvironmentVariable `json:"secrets"`
+	Secrets       []EnvironmentVariable `json:"secrets" gorm:"type:boolean"`
 	Services      []Service             `json:"services"`
 	Artifacts     postgres.Jsonb        `json:"artifacts" gorm:"type:jsonb;not null"`
 	Finished      time.Time
