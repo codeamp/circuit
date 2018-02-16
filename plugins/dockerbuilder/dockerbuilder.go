f7f57787e424469bb5e0d8139b7eec9e1e435158
Shreyas Jaganmohan <shreyas.jaganmohan@gmail.com>
2018-02-16 13:16:05 -0800

    Revert "Remove PASSWORD from being inserted into Artifacts (#78)" (#79)
    This reverts commit e16af6ab88564bbd843867cc1048a82ae49668b4.

--- plugins/dockerbuilder/dockerbuilder.go
+++ plugins/dockerbuilder/dockerbuilder.go
@@ -326,6 +326,7 @@ func (x *DockerBuilder) Process(e transistor.Event) error {
 	event.State = plugins.GetState("complete")
 	event.Artifacts["IMAGE"] = fullImagePath(event)
 	event.Artifacts["USER"] = event.Extension.Config["DOCKERBUILDER_USER"].(string)
+	event.Artifacts["PASSWORD"] = event.Extension.Config["DOCKERBUILDER_PASSWORD"].(string)
 	event.Artifacts["EMAIL"] = event.Extension.Config["DOCKERBUILDER_EMAIL"].(string)
 	event.Artifacts["HOST"] = event.Extension.Config["DOCKERBUILDER_HOST"].(string)
 	event.StateMessage = "Completed"
