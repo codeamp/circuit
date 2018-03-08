5a3a1044811b47d905811c00249ef02021f3e17d
Shreyas Jaganmohan <shreyas@checkr.com>
2018-03-08 12:57:08 -0800

    Add migration and modify initdb to include testdb

--- bootstrap/postgres/init-codeamp-db.sh
+++ bootstrap/postgres/init-codeamp-db.sh
@@ -4,8 +4,17 @@ set -e
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
 	CREATE USER codeamp;
   CREATE DATABASE codeamp;
+  CREATE DATABASE codeamp_test;
+
+	GRANT ALL PRIVILEGES ON DATABASE codeamp_test TO codeamp;
 	GRANT ALL PRIVILEGES ON DATABASE codeamp TO codeamp;
+   
   \c codeamp
   CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
   CREATE EXTENSION IF NOT EXISTS hstore;
+  
+  \c codeamp_test
+  CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+  CREATE EXTENSION IF NOT EXISTS hstore; 
+
 EOSQL
