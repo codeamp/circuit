96e5e43c8f5da162f050e305133e436d679aa70c
Amos Kyler <amos.kyler@gmail.com>
2018-05-03 14:29:07 -0700

    implement cancel release backend (#166)
    * implement cancel release backend
    
    * comment consistency & plugin.Action fix
    
    * id name in log consistency

--- plugins/codeamp/codeamp.go
+++ plugins/codeamp/codeamp.go
@@ -763,8 +763,16 @@ func (x *CodeAmp) WorkflowReleaseExtensionsCompleted(release *resolvers.Release)
 	}
 
 	for _, releaseExtension := range releaseExtensions {
+		releaseExtensionAction := plugins.Action("create")
 		if releaseExtension.Type == plugins.GetType("deployment") {
 			_artifacts := artifacts
+
+			// Fail deployment if the release is in a failed state
+			if release.State == plugins.GetState("failed") {
+				releaseExtensionAction = plugins.GetAction("status")
+				releaseExtension.State = plugins.GetState("failed")
+				releaseExtension.StateMessage = release.StateMessage
+			}
 
 			projectExtension := resolvers.ProjectExtension{}
 			if x.DB.Where("id = ?", releaseExtension.ProjectExtensionID).Find(&projectExtension).RecordNotFound() {
@@ -795,7 +803,7 @@ func (x *CodeAmp) WorkflowReleaseExtensionsCompleted(release *resolvers.Release)
 
 			releaseExtensionEvent := plugins.ReleaseExtension{
 				ID:     releaseExtension.Model.ID.String(),
-				Action: plugins.GetAction("create"),
+				Action: releaseExtensionAction,
 				Slug:   extension.Key,
 				State:  releaseExtension.State,
 				Release: plugins.Release{
