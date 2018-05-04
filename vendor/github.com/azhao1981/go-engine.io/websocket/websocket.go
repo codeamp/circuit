69a9be895dc970b9ff51f8f75a2d63a67c30852f
Saso Matejina <matejina@gmail.com>
2018-05-03 22:29:45 -0700

    this better works

---
+++ vendor/github.com/azhao1981/go-engine.io/websocket/websocket.go
@@ -0,0 +1,12 @@
+package websocket
+
+import (
+	"github.com/azhao1981/go-engine.io/transport"
+)
+
+var Creater = transport.Creater{
+	Name:      "websocket",
+	Upgrading: true,
+	Server:    NewServer,
+	Client:    NewClient,
+}
