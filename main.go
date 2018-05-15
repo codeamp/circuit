50625a0e5b89c7c8d83033eadf8b046b0bd255c7
Andrew Ballman <35912177+aballman@users.noreply.github.com>
2018-05-15 14:55:51 -0700

    LOG_LEVEL added to control log output (#191)
    * LOG_LEVEL env var now controls the level of log output from the internal logger
    
    * System now treats a LOG_LEVEL thats provided, yet invalid, as a FATAL event. This is in hopes that we never assign an invalid log level that does not behave the way we are expecting

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"os"
+
 	"github.com/codeamp/circuit/cmd"
 	_ "github.com/codeamp/circuit/plugins/codeamp"
 	_ "github.com/codeamp/circuit/plugins/dockerbuilder"
@@ -9,8 +11,20 @@ import (
 	_ "github.com/codeamp/circuit/plugins/heartbeat"
 	_ "github.com/codeamp/circuit/plugins/kubernetes"
 	_ "github.com/codeamp/circuit/plugins/route53"
+	log "github.com/codeamp/logger"
+	"github.com/sirupsen/logrus"
 )
 
 func main() {
+	if _logLevel := os.Getenv("LOG_LEVEL"); _logLevel != "" {
+		logLevel, err := logrus.ParseLevel(_logLevel)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		log.SetLogLevel(logLevel)
+	}
+
 	cmd.Execute()
 }
