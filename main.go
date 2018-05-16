ac7bcbc8a2bb5e76f0b8a862a6b1b1286401fe47
codeamp <codeamp@codeamp.com>
2018-05-15 20:25:55 -0700

    Moved log level setting to be in line with other viper configurations

--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"os"
-
 	"github.com/codeamp/circuit/cmd"
 	_ "github.com/codeamp/circuit/plugins/codeamp"
 	_ "github.com/codeamp/circuit/plugins/dockerbuilder"
@@ -11,20 +9,8 @@ import (
 	_ "github.com/codeamp/circuit/plugins/heartbeat"
 	_ "github.com/codeamp/circuit/plugins/kubernetes"
 	_ "github.com/codeamp/circuit/plugins/route53"
-	log "github.com/codeamp/logger"
-	"github.com/sirupsen/logrus"
 )
 
 func main() {
-	if _logLevel := os.Getenv("LOG_LEVEL"); _logLevel != "" {
-		logLevel, err := logrus.ParseLevel(_logLevel)
-
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		log.SetLogLevel(logLevel)
-	}
-
 	cmd.Execute()
 }
