d91eb1b53ecbf2711c4c9279dbff98919f1edcde
Shreyas Jaganmohan <shreyas@checkr.com>
2018-06-14 16:56:13 -0700

    Add pagination for other relevant resources

--- plugins/codeamp/resolvers/paginator.go
+++ plugins/codeamp/resolvers/paginator.go
@@ -523,3 +523,244 @@ func (r *SecretListResolver) getPage() int32 {
 
 	return 1
 }
+
+// SERVICES
+
+// SecretResolver resolver for Release
+type ServiceListResolver struct {
+	ServiceList    []Service
+	PaginatorInput *PaginatorInput
+	DB             *gorm.DB
+}
+
+// Services
+func (r *ServiceListResolver) Entries() []*ServiceResolver {
+	var filteredRows []Service
+	var results []*ServiceResolver
+
+	cursorRowIdx := 0
+
+	// filter on things after cursor_id
+	for idx, row := range r.ServiceList {
+		if r.PaginatorInput.Cursor != nil && row.Model.ID.String() == *r.PaginatorInput.Cursor {
+			cursorRowIdx = idx
+			break
+		}
+	}
+
+	i := cursorRowIdx
+	for {
+		if len(filteredRows) == int(r.PaginatorInput.Limit) ||
+			len(r.ServiceList) == i {
+			break
+		}
+		filteredRows = append(filteredRows, r.ServiceList[i])
+		i++
+	}
+
+	for _, row := range filteredRows {
+		results = append(results, &ServiceResolver{
+			DB:      r.DB,
+			Service: row,
+		})
+	}
+	return results
+}
+
+func (r *ServiceListResolver) Page() int32 {
+	// get page # from count / itemsPerPage
+	return r.getPage()
+}
+
+func (r *ServiceListResolver) Count() int32 {
+	return int32(len(r.ServiceList))
+}
+
+func (r *ServiceListResolver) NextCursor() string {
+	cursorRowIdx := 0
+
+	// filter on things after cursor_id
+	for idx, row := range r.ServiceList {
+		if r.PaginatorInput.Cursor != nil && row.Model.ID.String() == *r.PaginatorInput.Cursor {
+			cursorRowIdx = idx
+			break
+		}
+	}
+
+	nextCursorIdx := cursorRowIdx + int(r.PaginatorInput.Limit) + 1
+	if len(r.ServiceList) >= nextCursorIdx {
+		return r.ServiceList[nextCursorIdx].Model.ID.String()
+	} else {
+		return ""
+	}
+}
+
+func (r *ServiceListResolver) getPage() int32 {
+	for idx, row := range r.ServiceList {
+		if row.Model.ID.String() == *r.PaginatorInput.Cursor {
+			return int32(idx)/r.PaginatorInput.Limit + int32(1)
+		}
+	}
+
+	return 1
+}
+
+// FEATURES
+
+// SecretResolver resolver for Release
+type FeatureListResolver struct {
+	FeatureList    []Feature
+	PaginatorInput *PaginatorInput
+	DB             *gorm.DB
+}
+
+func (r *FeatureListResolver) Entries() []*FeatureResolver {
+	var filteredRows []Feature
+	var results []*FeatureResolver
+
+	cursorRowIdx := 0
+
+	// filter on things after cursor_id
+	for idx, row := range r.FeatureList {
+		if r.PaginatorInput.Cursor != nil && row.Model.ID.String() == *r.PaginatorInput.Cursor {
+			cursorRowIdx = idx
+			break
+		}
+	}
+
+	i := cursorRowIdx
+	for {
+		if len(filteredRows) == int(r.PaginatorInput.Limit) ||
+			len(r.FeatureList) == i {
+			break
+		}
+		filteredRows = append(filteredRows, r.FeatureList[i])
+		i++
+	}
+
+	for _, row := range filteredRows {
+		results = append(results, &FeatureResolver{
+			DB:      r.DB,
+			Feature: row,
+		})
+	}
+	return results
+}
+
+func (r *FeatureListResolver) Page() int32 {
+	// get page # from count / itemsPerPage
+	return r.getPage()
+}
+
+func (r *FeatureListResolver) Count() int32 {
+	return int32(len(r.FeatureList))
+}
+
+func (r *FeatureListResolver) NextCursor() string {
+	cursorRowIdx := 0
+
+	// filter on things after cursor_id
+	for idx, row := range r.FeatureList {
+		if r.PaginatorInput.Cursor != nil && row.Model.ID.String() == *r.PaginatorInput.Cursor {
+			cursorRowIdx = idx
+			break
+		}
+	}
+
+	nextCursorIdx := cursorRowIdx + int(r.PaginatorInput.Limit) + 1
+	if len(r.FeatureList) >= nextCursorIdx {
+		return r.FeatureList[nextCursorIdx].Model.ID.String()
+	} else {
+		return ""
+	}
+}
+
+func (r *FeatureListResolver) getPage() int32 {
+	for idx, row := range r.FeatureList {
+		if row.Model.ID.String() == *r.PaginatorInput.Cursor {
+			return int32(idx)/r.PaginatorInput.Limit + int32(1)
+		}
+	}
+
+	return 1
+}
+
+// PROJECTS
+
+// SecretResolver resolver for Release
+type ProjectListResolver struct {
+	ProjectList    []Project
+	PaginatorInput *PaginatorInput
+	DB             *gorm.DB
+}
+
+func (r *ProjectListResolver) Entries() []*ProjectResolver {
+	var filteredRows []Project
+	var results []*ProjectResolver
+
+	cursorRowIdx := 0
+
+	// filter on things after cursor_id
+	for idx, row := range r.ProjectList {
+		if r.PaginatorInput.Cursor != nil && row.Model.ID.String() == *r.PaginatorInput.Cursor {
+			cursorRowIdx = idx
+			break
+		}
+	}
+
+	i := cursorRowIdx
+	for {
+		if len(filteredRows) == int(r.PaginatorInput.Limit) ||
+			len(r.ProjectList) == i {
+			break
+		}
+		filteredRows = append(filteredRows, r.ProjectList[i])
+		i++
+	}
+
+	for _, row := range filteredRows {
+		results = append(results, &ProjectResolver{
+			DB:      r.DB,
+			Project: row,
+		})
+	}
+	return results
+}
+
+func (r *ProjectListResolver) Page() int32 {
+	// get page # from count / itemsPerPage
+	return r.getPage()
+}
+
+func (r *ProjectListResolver) Count() int32 {
+	return int32(len(r.ProjectList))
+}
+
+func (r *ProjectListResolver) NextCursor() string {
+	cursorRowIdx := 0
+
+	// filter on things after cursor_id
+	for idx, row := range r.ProjectList {
+		if r.PaginatorInput.Cursor != nil && row.Model.ID.String() == *r.PaginatorInput.Cursor {
+			cursorRowIdx = idx
+			break
+		}
+	}
+
+	nextCursorIdx := cursorRowIdx + int(r.PaginatorInput.Limit) + 1
+	if len(r.ProjectList) >= nextCursorIdx {
+		return r.ProjectList[nextCursorIdx].Model.ID.String()
+	} else {
+		return ""
+	}
+}
+
+func (r *ProjectListResolver) getPage() int32 {
+	for idx, row := range r.ProjectList {
+		if row.Model.ID.String() == *r.PaginatorInput.Cursor {
+			return int32(idx)/r.PaginatorInput.Limit + int32(1)
+		}
+	}
+
+	return 1
+}
