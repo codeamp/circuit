c1a88a88e7ce5be8f787426340fbfe7fe8bd55a1
Shreyas <shreyas.jaganmohan@gmail.com>
2018-08-29 15:51:15 -0700

    add release event

--- plugins/codeamp/graphql/mutation.go
+++ plugins/codeamp/graphql/mutation.go
@@ -761,7 +761,7 @@ func (r *Resolver) CreateRelease(ctx context.Context, args *struct{ Release *mod
 			release.Started = time.Now()
 			r.DB.Save(&release)
 
-			spew.Dump("sending event")
+			spew.Dump("sending event", releaseEvent)
 			r.Events <- transistor.NewEvent(transistor.EventName("release"), transistor.GetAction("create"), releaseEvent)
 		} else {
 			log.Info(fmt.Sprintf("Release is already running, queueing %s", release.Model.ID.String()))
