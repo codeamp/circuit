18db602a55edccc241248f608206a2589019d1e2
Shreyas <shreyas.jaganmohan@gmail.com>
2018-11-08 10:05:20 -0800

    Add default property for service spec profiles

--- plugins/codeamp/graphql/servicespec_mutation.go
+++ plugins/codeamp/graphql/servicespec_mutation.go
@@ -92,6 +92,15 @@ func (r *ServiceSpecResolverMutation) UpdateServiceSpec(args *struct{ ServiceSpe
 	if serviceSpec.Model.ID.String() == currentDefault.Model.ID.String() {
 		isDefault = true
 	}
+	
+	// if IsDefault is True, check which one is the current default
+	if args.ServiceSpec.IsDefault {
+		var currentDefault model.ServiceSpec
+		if err := r.DB.Where("is_default = ?", true).First(&currentDefault).Error; err == nil {
+			currentDefault.IsDefault = false			
+			r.DB.Save(&currentDefault)
+		}
+	}	
 
 	serviceSpec.Name = args.ServiceSpec.Name
 	serviceSpec.CpuLimit = args.ServiceSpec.CpuLimit
