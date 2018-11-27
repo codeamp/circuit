36ab4bbc6b45f98136cbf6f0c161ed17cda27f05
Shreyas <shreyas.jaganmohan@gmail.com>
2018-11-27 13:54:48 -0800

    Add name resolver

--- plugins/codeamp/db/servicespec.go
+++ plugins/codeamp/db/servicespec.go
@@ -1,13 +1,42 @@
 package db_resolver
 
 import (
+	"fmt"
+
 	"github.com/codeamp/circuit/plugins/codeamp/model"
+	log "github.com/codeamp/logger"
 	"github.com/jinzhu/gorm"
 )
 
 type ServiceSpecResolver struct {
 	model.ServiceSpec
 	DB *gorm.DB
+}
+
+// Name
+func (r *ServiceSpecResolver) Name() string {
+	env := model.Environment{}
+	project := model.Project{}
+	service := model.Service{}
+ 	if err := r.DB.Where("id = ?", r.ServiceSpec.ServiceID).First(&service).Error; err != nil {
+		log.InfoWithFields("service not found", log.Fields{
+			"id": r.ServiceSpec.ServiceID,
+		})
+		return r.ServiceSpec.Name
+	}
+ 	if err := r.DB.Where("id = ?", service.ProjectID).First(&project).Error; err != nil {
+		log.InfoWithFields("project not found", log.Fields{
+			"id": service.ProjectID,
+		})
+		return r.ServiceSpec.Name
+	}
+ 	if err := r.DB.Where("id = ?", service.EnvironmentID).First(&env).Error; err != nil {
+		log.InfoWithFields("environment not found", log.Fields{
+			"id": service.EnvironmentID,
+		})
+		return r.ServiceSpec.Name
+	}
+ 	return fmt.Sprintf("%s/%s/%s", service.Name, project.Slug, env.Key)
 }
 
 func (r *ServiceSpecResolver) Service() *ServiceResolver {
