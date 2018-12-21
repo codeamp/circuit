2ce48f2687fba5c2d71cb528e1a1b80e31eb6dee
Andrew Ballman <andrew.ballman@checkr.com>
2018-12-20 17:04:39 -0800

    Fixed bad merge commit

--- plugins/codeamp/graphql/release_mutation.go
+++ plugins/codeamp/graphql/release_mutation.go
@@ -554,8 +554,6 @@ func (r *ReleaseResolverMutation) getProjectExtensions(projectID string, environ
 	}
 
 	return projectExtensions, nil
-		release.State = transistor.GetState("running")
-		release.StateMessage = "Running Release"
 }
 
 func (r *ReleaseResolverMutation) StopRelease(ctx context.Context, args *struct{ ID graphql.ID }) (*ReleaseResolver, error) {
