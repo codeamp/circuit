d2a3b05f3f0cd236f571696d3353a1a9fb8cd5fa
Amos Kyler <amos.kyler@gmail.com>
2019-02-11 12:52:14 -0800

    - Removes influxdbdata vendor package (#431)
    - Adds gokong vendor package
    - Pins development postgres docker image to version 10.x

---
+++ vendor/github.com/kevholditch/gokong/status_test.go
@@ -0,0 +1,16 @@
+package gokong
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_GetStatus(t *testing.T) {
+	result, err := NewClient(NewDefaultConfig()).Status().Get()
+
+	assert.Nil(t, err)
+	assert.NotNil(t, result)
+	assert.True(t, result.Database.Reachable)
+	assert.True(t, result.Server.ConnectionsAccepted >= 1)
+}
