8cf181be701c66d1e845bd6fbe0e837babf543a5
Shreyas <shreyas.jaganmohan@gmail.com>
2019-02-25 16:57:59 -0800

    Add comments

--- plugins/codeamp/resourceconfig/resourceconfig.go
+++ plugins/codeamp/resourceconfig/resourceconfig.go
@@ -1,5 +1,10 @@
 package resourceconfig
 
+// A ResourceConfig is an object that is used for importing and exporting
+// resources in CodeAmp. Its purpose is to decouple implementation behaviors
+// of Export and Import from objects when the respective operation is occurring
+// and provide a general-purpose interface for any resource that wants to implement
+// import/export behaviors.
 type ResourceConfig interface {
 	Import() error
 	Export() (interface{}, error)
