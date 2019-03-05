adf1d695cbd14a2032d939b2480c91ffd202e26d
Shreyas <shreyas.jaganmohan@gmail.com>
2019-03-05 15:10:46 -0800

    Add functional version for postgres extension

--- plugins/database/database.go
+++ plugins/database/database.go
@@ -45,15 +45,16 @@ func (x *Database) Stop() {
 // Subscribe
 func (x *Database) Subscribe() []string {
 	return []string{
-		"project:database",
+		"project:database:create",
+		"project:database:delete",
 	}
 }
 
-func getFailedStatusEvent(err error) transistor.Event {
+func (x *Database) sendFailedStatusEvent(err error) {
 	ev := transistor.NewEvent(plugins.GetEventName("project:database"), transistor.GetAction("status"), nil)
 	ev.State = transistor.GetState("failed")
 	ev.StateMessage = err.Error()
-	return ev
+	x.events <- ev
 }
 
 // Process
@@ -62,80 +63,96 @@ func (x *Database) Process(e transistor.Event) error {
 
 	// confirm all required structured inputs (Project slug, Environment)
 	if e.PayloadModel != "plugins.ProjectExtension" {
-		err := errors.New("invalid payload model")
-		log.Error(err.Error())
-		x.events <- getFailedStatusEvent(err)
-		return err
+		x.sendFailedStatusEvent(errors.New("invalid payload model"))
+		return nil
 	}
 
 	projectExtensionEvent := e.Payload.(plugins.ProjectExtension)
 
-	// Create DB within shared instance of the correct db variant (postgres/mysql)
 	instanceEndpoint, err := e.GetArtifact("SHARED_DATABASE_HOST")
 	if err != nil {
-		x.events <- getFailedStatusEvent(err)
-		return err
+		x.sendFailedStatusEvent(err)
+		return nil
 	}
 
 	instanceUsername, err := e.GetArtifact("SHARED_DATABASE_ADMIN_USERNAME")
 	if err != nil {
-		x.events <- getFailedStatusEvent(err)
-		return err
+		x.sendFailedStatusEvent(err)
+		return nil
 	}
 
 	instancePassword, err := e.GetArtifact("SHARED_DATABASE_ADMIN_PASSWORD")
 	if err != nil {
-		x.events <- getFailedStatusEvent(err)
-		return err
+		x.sendFailedStatusEvent(err)
+		return nil
 	}
 
 	instancePort, err := e.GetArtifact("SHARED_DATABASE_PORT")
 	if err != nil {
-		x.events <- getFailedStatusEvent(err)
-		return err
+		x.sendFailedStatusEvent(err)
+		return nil
 	}
 
 	dbType, err := e.GetArtifact("DB_TYPE")
 	if err != nil {
-		x.events <- getFailedStatusEvent(err)
-		return err
+		x.sendFailedStatusEvent(err)
+		return nil
 	}
 
 	dbInstance, err := initDBInstance(dbType.String(), instanceEndpoint.String(), instanceUsername.String(), instancePassword.String(), instancePort.String())
 	if err != nil {
-		x.events <- getFailedStatusEvent(err)
-		return err
+		x.sendFailedStatusEvent(err)
+		return nil
 	}
 
-	dbUsername, err := genDBUser(projectExtensionEvent)
-	if err != nil {
-		x.events <- getFailedStatusEvent(err)
-		return err
+	// Create DB within shared instance of the correct db variant (postgres/mysql)
+	switch e.Action {
+	case transistor.GetAction("create"):
+		dbUsername := genDBUser(projectExtensionEvent)
+		dbName := genDBName(projectExtensionEvent)
+		dbPassword := genDBPassword()
+
+		dbMetadata, err := (*dbInstance).CreateDatabaseAndUser(dbName, dbUsername, dbPassword)
+		if err != nil {
+			x.sendFailedStatusEvent(err)
+			return nil
+		}
+
+		// store db metadata into instance
+		respEvent := transistor.NewEvent(e.Name, transistor.GetAction("status"), nil)
+		respEvent.State = transistor.GetState("complete")
+
+		respEvent.AddArtifact("DB_USER", dbMetadata.Credentials.Username, false)
+		respEvent.AddArtifact("DB_PASSWORD", dbMetadata.Credentials.Password, false)
+		respEvent.AddArtifact("DB_NAME", dbMetadata.Name, false)
+		respEvent.AddArtifact("DB_ENDPOINT", (*dbInstance).GetInstanceMetadata().Endpoint, false)
+		respEvent.AddArtifact("DB_PORT", (*dbInstance).GetInstanceMetadata().Port, false)
+
+		x.events <- respEvent
+	case transistor.GetAction("delete"):
+		dbName, err := e.GetArtifact("DB_NAME")
+		if err != nil {
+			x.sendFailedStatusEvent(err)
+			return nil
+		}
+
+		dbUser, err := e.GetArtifact("DB_USER")
+		if err != nil {
+			x.sendFailedStatusEvent(err)
+			return nil
+		}
+
+		if err = (*dbInstance).DeleteDatabaseAndUser(dbName.String(), dbUser.String()); err != nil {
+			x.sendFailedStatusEvent(err)
+			return nil
+		}
+
+		// store db metadata into instance
+		respEvent := transistor.NewEvent(e.Name, transistor.GetAction("status"), nil)
+		respEvent.State = transistor.GetState("complete")
+
+		x.events <- respEvent
 	}
 
-	dbPassword := ""
-	dbName, err := genDBName(projectExtensionEvent)
-	if err != nil {
-		x.events <- getFailedStatusEvent(err)
-		return err
-	}
-
-	dbMetadata, err := (*dbInstance).CreateDatabase(*dbName, *dbUsername, dbPassword)
-	if err != nil {
-		x.events <- getFailedStatusEvent(err)
-		return err
-	}
-
-	// store db metadata into instance
-	respEvent := transistor.NewEvent(e.Name, transistor.GetAction("status"), nil)
-	respEvent.State = transistor.GetState("complete")
-
-	respEvent.AddArtifact("DB_USER", dbMetadata.Credentials.Username, false)
-	respEvent.AddArtifact("DB_PASSWORD", dbMetadata.Credentials.Password, false)
-	respEvent.AddArtifact("DB_NAME", dbMetadata.Name, false)
-	respEvent.AddArtifact("DB_ENDPOINT", (*dbInstance).GetInstanceMetadata().Endpoint, false)
-	respEvent.AddArtifact("DB_PORT", (*dbInstance).GetInstanceMetadata().Port, false)
-
-	x.events <- respEvent
 	return nil
 }
