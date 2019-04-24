5eb20840aaf338ea75310ea794437814fd8cde67
Shreyas <shreyas.jaganmohan@gmail.com>
2019-04-24 16:19:18 -0700

    rm logmode and add unique

--- plugins/database/helpers.go
+++ plugins/database/helpers.go
@@ -7,20 +7,43 @@ import (
 	"time"
 
 	"github.com/codeamp/circuit/plugins"
+	uuid "github.com/satori/go.uuid"
 )
 
 // genDBName creates a database name for the specified
 // project extension with the format <project.slug>-<environment>
 func genDBName(pe plugins.ProjectExtension) string {
 	projectSlugWithUnderscores := strings.Replace(pe.Project.Slug, "-", "_", -1)
-	return fmt.Sprintf("%s_%s", projectSlugWithUnderscores, pe.Environment)
+	envWithUnderscores := strings.Replace(pe.Environment, "-", "_", -1)
+	if len(projectSlugWithUnderscores) > 20 {
+		projectSlugWithUnderscores = projectSlugWithUnderscores[:20]
+	}
+
+	if len(envWithUnderscores) > 20 {
+		envWithUnderscores = envWithUnderscores[:20]
+	}
+
+	uniqueID := uuid.NewV4()
+
+	return fmt.Sprintf("%s_%s_%s", projectSlugWithUnderscores, envWithUnderscores, strings.Replace(uniqueID.String()[:15], "-", "_", -1))
 }
 
 // genDBUsername creates a database username for the specified
 // project extension with the format <project.slug>-<environment>
 func genDBUser(pe plugins.ProjectExtension) string {
 	projectSlugWithUnderscores := strings.Replace(pe.Project.Slug, "-", "_", -1)
-	return fmt.Sprintf("%s_%s_user", projectSlugWithUnderscores, pe.Environment)
+	envWithUnderscores := strings.Replace(pe.Environment, "-", "_", -1)
+	if len(projectSlugWithUnderscores) > 20 {
+		projectSlugWithUnderscores = projectSlugWithUnderscores[:20]
+	}
+
+	if len(pe.Environment) > 20 {
+		envWithUnderscores = envWithUnderscores[:20]
+	}
+
+	uniqueID := uuid.NewV4()
+
+	return fmt.Sprintf("%s_%s_%s_user", projectSlugWithUnderscores, envWithUnderscores, strings.Replace(uniqueID.String()[:11], "-", "_", -1))
 }
 
 func genDBPassword() string {
