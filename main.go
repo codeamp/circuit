aae2d31e357625c91ace9eec11462d8ccca73d01
Andrew Ballman <35912177+aballman@users.noreply.github.com>
2019-05-03 16:02:46 -0700

    Mongo Self Service Plugin (#467)
    * Added mongo plugin
    
    * WIP Mongo
    
    * Small s3 updates
    
    * Added mongo plugin to main
    
    * Added event name to list of events
    
    * Mongo plugin now checks credentials to verify they were created successfully
    
    * Some comments added

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	_ "github.com/codeamp/circuit/plugins/gitsync"
 	_ "github.com/codeamp/circuit/plugins/heartbeat"
 	_ "github.com/codeamp/circuit/plugins/kubernetes"
+	_ "github.com/codeamp/circuit/plugins/mongo"
 	_ "github.com/codeamp/circuit/plugins/route53"
 	_ "github.com/codeamp/circuit/plugins/s3"
 	_ "github.com/codeamp/circuit/plugins/slack"
