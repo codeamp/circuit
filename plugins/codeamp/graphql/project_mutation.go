9507a2ef6bf8636d3dab483c90dd52cc856f572f
Shreyas <shreyas.jaganmohan@gmail.com>
2019-09-16 16:35:47 -0700

    uh-oh other way around

--- plugins/codeamp/graphql/project_mutation.go
+++ plugins/codeamp/graphql/project_mutation.go
@@ -187,7 +187,7 @@ func (r *ProjectResolverMutation) UpdateProject(ctx context.Context, args *struc
 
 	// Check if user has "admin" level permissions and whether GitBranch is not nil
 	_, noAdminPermissionErr := auth.CheckAuth(ctx, []string{"admin"})
-	if args.Project.GitBranch != nil && noAdminPermissionErr != nil {
+	if args.Project.GitBranch != nil && noAdminPermissionErr == nil {
 		projectID, err := uuid.FromString(*args.Project.ID)
 		if err != nil {
 			return nil, fmt.Errorf("Couldn't parse project ID")
